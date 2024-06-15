91692a00db3fb2a7b9bc3d37f2672f476f8126da
Steven <gonghaima@hotmail.com>
2024-06-16 00:35:46 +1000

    add dbContext

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
--- PlatformService/readme.md
+++ PlatformService/readme.md
@@ -9,4 +9,5 @@ dotnet add package Microsoft.EntityFrameworkCore
 dotnet add package Microsoft.EntityFrameworkCore.Design
 dotnet add package Microsoft.EntityFrameworkCore.InMemory
 dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
 
