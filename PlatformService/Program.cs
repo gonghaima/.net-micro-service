6e3107cc584d36a01a1c5b5491e35860967799d2
Steven <gonghaima@hotmail.com>
2024-06-19 15:31:15 +1000

    Dtos & Profile

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMem"));
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
